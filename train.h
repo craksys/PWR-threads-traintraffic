81310894dc977f1c2dec4f36bfb3c13021f76ece
craksys <skkf0004@gmail.com>
2023-04-01 18:51:06 +0200

    fixed train.h

--- train.h
+++ train.h
@@ -6,17 +6,12 @@
 class Train{
 public:
     void createOutsideTrain();
-
     void createOutsideTrain(railStruct *rails_array, int start_position, int speed);
-
-    void printError(std::string error_name);
-
     bool isCrossover(railStruct *rails_array, int position);
-
+private:
     void sleepTrain(int speed) const;
-
     bool isGreenLight(railStruct *arr, int position);
+    void printError(std::string error_name);
 };
-
 
 #endif //UNTITLED_TRAIN_H
